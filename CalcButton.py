d18c41d63d7da53b4baa2185ee8b0ff7143aa661
Sean O'Dea <60398405+sodea9@users.noreply.github.com>
2023-08-11 00:51:47 -0400

    Resized buttons to proper squares, deleted CalcButton class

--- CalcButton.py
+++
@@ -1,5 +0,0 @@
-from tkinter import Button
-
-class CalcButton(Button):
-    def __init__(self, masterWidget, text=None, image=None):
-        super().__init__(masterWidget, text=text, image=image, width=8, height=5)
