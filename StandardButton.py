8e4c868a6c26980952cb0d578687492438345aa0
Sean O'Dea <60398405+sodea9@users.noreply.github.com>
2023-08-16 22:24:06 -0400

    Configured default sizing for buttons using width and padding

--- StandardButton.py
+++
@@ -1,8 +0,0 @@
-from tkinter.ttk import Button
-
-BUTTONWIDTH = 5
-BUTTONHEIGHT = 60
-
-class StandardButton(Button):
-    def __init__(self, parentWidget, text=None, image=None):
-        super().__init__(parentWidget, text=text, image=image, width=BUTTONWIDTH, compound="c")
