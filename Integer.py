180145732d4ccbb93fa93e53867cc231aea8360b
Sean O'Dea <60398405+sodea9@users.noreply.github.com>
2023-09-11 22:59:31 -0400

    cleaning up

--- Integer.py
+++
@@ -1,9 +0,0 @@
-class Integer():
-    def __init__(self, value=0):
-        self.value = value
-
-    def get(self):
-        return self.value
-    
-    def set(self, newValue):
-        self.value = newValue
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Calculator
 A basic calculator built using the Python Tkinter library to practice UI and app dev
+
+*Currently a nonfunctional work in progress*
+
+![Alt text](screenshot.png)
